304e2368f384b30ad91128097d71d7653aaf9188
BestChennn <bestchenbestchen@gmail.com>
2023-02-21 20:29:17 +0700

    Complete

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,28 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
 
+	double overX,overY,overresult;
+
+	overX = min(R1.x+R1.w,R2.x+R2.w) - max(R1.x,R2.x) ; 
+	overY = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h)  ; 
+
+	overresult = overX*overY;
+
+
+	if(overresult<=0){
+		return 0;
+	}else{
+		return overresult;
+	}
 
 
 }
+
+int main(){
+Rect R1 = {-1,2,6.9,9.6};  
+Rect R2 = {0,0,1.2,2.5};   
+cout << overlap(R1,R2);
+}
+
